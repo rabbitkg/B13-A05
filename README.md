d362831d2508f5ed4e4d95137dac856fb6747e3b
rabbitkg <rabbitkgbd@gmail.com>
2026-03-09 10:17:10 +0600

    added responsive and creait more style.

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 
     - 1️⃣ What is the difference between var, let, and const?
+    Ans: var is a function and its can re-declared and updated.
+
     - 2️⃣ What is the spread operator (...)?
+    Ans: In JavaScript the spread operator (...) is used to expand elements of an array/object into individual elements.
+
     - 3️⃣ What is the difference between map(), filter(), and forEach()?
+    Ans: map() returns New array and transforms each element, filter() returns elements that pass a condition and forEach() returns undefined and executes a function on element.
+
     - 4️⃣ What is an arrow function?
+    Ans: Arrow function in JavaScript is a syntax for writing function.
+
     - 5️⃣ What are template literals?
+    Ans: Its create strings using backticks (``). They allow variables and expressions easily.
 
 
 
--- script/main.js
+++ script/main.js
@@ -75,17 +75,82 @@ const loadIssueDetails = (id) => {
 };
 
 const displayIssueModal = (issue) => {
-    document.getElementById("modal-title").innerText = issue.title;
-    document.getElementById("modal-description").innerText = issue.description;
-    document.getElementById("modal-status").innerText = issue.status;
-    document.getElementById("modal-category").innerText = issue.category;
-    document.getElementById("modal-author").innerText = issue.author;
-    document.getElementById("modal-priority").innerText = issue.priority;
-    document.getElementById("modal-label").innerText =
-    issue.labels?.map(l => l.toUpperCase()).join(", ") || "NO LABEL";
-    document.getElementById("modal-data").innerText = issue.createdAt;
+
+   
+
+    const assignee =
+    issue.author.charAt(0).toUpperCase() + issue.author.slice(1);
+
+
+    let priorityStyle = "bg-red-400 text-white px-3 py-1 rounded-full text-xs font-semibold flex items-center justify-center";
+
+    if(issue.priority === "medium"){
+        priorityStyle = "bg-yellow-300 text-black px-3 py-1 rounded-full text-xs font-semibold";
+    }
+    else if(issue.priority === "low"){
+        priorityStyle = "bg-gray-200 text-gray-700 px-3 py-1 rounded-full text-xs font-semibold";
+    }
+
+    let statusStyle = "bg-green-600 text-white";
+
+    if(issue.status === "closed"){
+        statusStyle = "bg-[#A855F7] text-white";
+    };
+
+    const labels = issue.labels || [];
+
+    const labelsHTML = labels.map(label => {
+        let color = "bg-red-200 text-red-600";
+
+        if(label.toLowerCase() === "help wanted"){
+            color = "bg-yellow-200 text-yellow-700";
+        }
+        else if(label.toLowerCase() === "enhancement"){
+            color = "bg-green-200 text-green-700";
+        }
+
+        return `<span class="badge ${color}">${label.toUpperCase()}</span>`;
+    }).join("");
+
+    document.querySelector(".modal-box").innerHTML = `
+        <h3 class="text-xl font-bold mb-2">${issue.title}</h3>
+
+        <div class="flex items-center gap-2 font-bold text-sm mb-3">
+            <span class="badge ${statusStyle}">
+                ${issue.status.toUpperCase()}
+            </span>
+            <span>Opened by <b>${issue.author}</b></span>
+            <span class="text-gray-400">${issue.createdAt}</span>
+        </div>
+
+        <div class="flex gap-2 mb-4 flex-wrap font-bold">
+            ${labelsHTML}
+        </div>
+
+        <p class="text-gray-600 mb-6">
+            ${issue.description}
+        </p>
+
+        <div class="bg-gray-100 p-4 gap-[37%] rounded-lg flex text-sm">
+            <div>
+                <p class="text-gray-500">Assignee</p>
+                <p class="font-semibold">${assignee}</p>
+            </div>
+
+            <div>
+                <p class="text-gray-500">Priority</p>
+                <span class="${priorityStyle}">${issue.priority.toUpperCase()}</span>
+            </div>
+        </div>
+
+        <div class="modal-action">
+            <form method="dialog">
+                <button class="btn btn-primary">Close</button>
+            </form>
+        </div>
+    `;
+
     document.getElementById("issue_modal").showModal();
-    
 };
 
 
@@ -106,6 +171,20 @@ const loadIssues = () => {
 
     },200);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const displayIssues = (issues) => {
@@ -156,19 +235,19 @@ const displayIssues = (issues) => {
         let labelsHTML = labels.map(label => {
 
         let labelLower = label.toLowerCase();
-        let labelColor = "bg-red-200 font-bold text-red-600";
+        let labelColor = "bg-red-200 font-bold text-red-500";
 
         if(labelLower === "help wanted"){
             labelColor = "bg-yellow-200 font-bold text-yellow-600";
         }
         else if(labelLower === "enhancement"){
 
-            labelColor = "bg-green-200 font-bold text-green-600";
+            labelColor = "bg-green-200 font-bold text-green-500";
 
         }
 
         return `
-            <span class="badge ${labelColor} whitespace-nowrap">
+            <span class="badge ${labelColor}">
                 ${label.toUpperCase()}
             </span>
         `;
@@ -195,7 +274,7 @@ const displayIssues = (issues) => {
         card.innerHTML = `
 
             <div onclick="loadIssueDetails(${issue.id})"
-            class="bg-white p-4 rounded-lg shadow border-t-4 ${borderColor} cursor-pointer hover:shadow-md transition">
+            class="bg-white p-4 rounded-lg shadow border-t-4 ${borderColor} cursor-pointer  shadow-lg hover:shadow-xl transition h-full flex flex-col justify-between">
 
                 <div class="flex justify-between items-center mb-2">
 
@@ -213,11 +292,11 @@ const displayIssues = (issues) => {
                 ${issue.title}
                 </h2>
 
-                <p class="text-sm text-gray-500 mb-3">
+                <p class="text-sm text-gray-500 mb-3 line-clamp-3">
                 ${issue.description?.slice(0,70)}...
                 </p>
 
-                <div class="flex gap-2 mb-3">
+                <div class="flex flex-wrap gap-2 mb-3">
 
                    ${labelsHTML}
 
